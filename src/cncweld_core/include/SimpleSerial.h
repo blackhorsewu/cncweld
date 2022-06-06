3d5df4677ab68dfb9205c4bbc65dcf067017ef7f
blackhorsewu <victor.wh.wu@gmail.com>
2022-06-06 18:22:09 +0800

    Revert "cncweld_core_node can now work"
    This reverts commit 3aa6fc9cf68731b89b2dbd255b0fe1d06d4a1135.

---
+++ src/cncweld_core/include/SimpleSerial.h
@@ -0,0 +1,47 @@
+/* 
+ * File:   SimpleSerial.h
+ * Author: Terraneo Federico
+ * Distributed under the Boost Software License, Version 1.0.
+ *
+ * Created on September 10, 2009, 12:12 PM
+ */
+
+#ifndef _SIMPLESERIAL_H
+#define	_SIMPLESERIAL_H
+
+#include <boost/asio.hpp>
+
+class SimpleSerial
+{
+public:
+    /**
+     * Constructor.
+     * \param port device name, example "/dev/ttyUSB0" or "COM4"
+     * \param baud_rate communication speed, example 9600 or 115200
+     * \throws boost::system::system_error if cannot open the
+     * serial device
+     */
+    SimpleSerial(std::string port, unsigned int baud_rate);
+
+    /**
+     * Write a string to the serial device.
+     * \param s string to write
+     * \throws boost::system::system_error on failure
+     */
+    void writeString(std::string s);
+
+    /**
+     * Blocks until a line is received from the serial device.
+     * Eventual '\n' or '\r\n' characters at the end of the string are removed.
+     * \return a string containing the received line
+     * \throws boost::system::system_error on failure
+     */
+    std::string readLine();
+
+private:
+    boost::asio::io_service io;
+    boost::asio::serial_port serial;
+};
+
+#endif	/* _SIMPLESERIAL_H */
+
--- src/cncweld_core/src/cncweld_core_node.cpp
+++ src/cncweld_core/src/cncweld_core_node.cpp
@@ -1,80 +1,163 @@
 
-#include "AsyncSerial.h"
+#include <ros/ros.h>
 
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <termios.h>
 
+#include "AsyncSerial.h"
 using namespace std;
 
-void received(const char *data, unsigned int len)
+// ros parameters
+string devname;
+int baudrate;
+
+int xaccel, yaccel, zaccel;
+int xmin, ymin, zmin;
+int xmax, ymax, zmax;
+int xspeed, yspeed, zspeed;
+int xsteps, ysteps, zsteps;
+
+// static int serial;
+struct termios SerialPortSettings;
+
+CallbackAsyncSerial serial("/dev/ttyACM0", 115200);
+
+int getrosparams(ros::NodeHandle pnh)
 {
-    vector<char> v(data,data+len);
-    for(unsigned int i=0;i<v.size();i++)
+  pnh.getParam("port", devname);
+  pnh.getParam("baudrate", baudrate);
+
+  pnh.getParam("x_accel", xaccel);
+  pnh.getParam("y_accel", xaccel);
+  pnh.getParam("z_accel", xaccel);
+
+  pnh.getParam("x_min", xmin);
+  pnh.getParam("y_min", ymin);
+  pnh.getParam("z_min", zmin);
+
+  pnh.getParam("x_max", xmax);
+  pnh.getParam("y_max", zmax);
+  pnh.getParam("z_max", zmax);
+
+  pnh.getParam("x_max_speed", xspeed);
+  pnh.getParam("y_max_speed", yspeed);
+  pnh.getParam("z_max_speed", zspeed);
+
+  pnh.getParam("x_steps", xsteps);
+  pnh.getParam("y_steps", ysteps);
+  pnh.getParam("z_steps", zsteps);
+
+  return(1);
+}
+
+int setGrbl()
+{
+
+  try {
+/*
+    // setup the serial port first
+    serial = open(devname.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    tcgetattr(serial, &SerialPortSettings);
+    cfsetspeed(&SerialPortSettings, B115200); // Set baudrate
+
+    string out_msg;
+    out_msg = "$100=" + to_string(xsteps) + "\n";
+    // set the steps per mm
+    write(serial, out_msg.c_str(), strlen(out_msg.c_str()));
+*/    
+    serial.writeString("$100=" + to_string(xsteps) + "\n");
+    serial.writeString("$101=" + to_string(ysteps) + "\n");
+    serial.writeString("$102=" + to_string(zsteps) + "\n");
+
+    // set the maximum speed mm/min
+    serial.writeString("$110=" + to_string(xspeed) + "\n");
+    serial.writeString("$111=" + to_string(yspeed) + "\n");
+    serial.writeString("$112=" + to_string(zspeed) + "\n");
+
+    // set the maximum acceleration mm/sec^2
+    serial.writeString("$120=" + to_string(xaccel) + "\n");
+    serial.writeString("$121=" + to_string(yaccel) + "\n");
+    serial.writeString("$122=" + to_string(zaccel) + "\n");
+
+    // Set the maximum travel distance mm
+    serial.writeString("$130=" + to_string(xmax - xmin) + "\n");
+    serial.writeString("$131=" + to_string(ymax - ymin) + "\n");
+    serial.writeString("$132=" + to_string(zmax - zmin) + "\n");
+
+/*
+    for(;;)
     {
-        if(v[i]=='\n')
+      if(serial.errorStatus() || serial.isOpen()==false)
+      {
+        cerr<<"Error: serial port unexpectedly closed"<<endl;
+        break;
+      }
+      char c;
+      cin.get(c); //blocking wait for standard input
+      if(c==3) //if Ctrl-C
+      {
+        cin.get(c);
+        switch(c)
         {
-            cout<<endl;
-        } else {
-            if(v[i]<32 || v[i]>=0x7f) cout.put(' ');//Remove non-ascii char
-            else cout.put(v[i]);
+          case 3:
+            serial.write(&c,1);//Ctrl-C + Ctrl-C, send Ctrl-C
+            break;
+          case 'x': //fall-through
+          case 'X':
+            goto quit;//Ctrl-C + x, quit
+          default:
+            serial.write(&c,1);//Ctrl-C + any other char, ignore
         }
+      } else serial.write(&c,1);
     }
-    cout.flush();//Flush screen buffer
+    // quit:
+    // serial.close();
+*/  
+  } catch (std::exception& e) {
+    cerr<<"Exception: "<<e.what()<<endl;
+  }
+
+  // tcsetattr(0, TCSANOW, &stored_settings);
+
+  return(1);
+}
+
+int homeGrbl()
+{
+
+  serial.writeString("$H\n");
+
+  return(1);
+
 }
 
 int main(int argc, char* argv[])
 {
-    if(argc!=3)
-    {
-        cerr<<"Usage: serial port baudrate"<<endl<<
-                "To quit type Ctrl-C x"<<endl<<
-                "To send Ctrl-C type Ctrl-C Ctrl-C"<<endl;
-        return 1;
-    }
+  ros::init(argc, argv, "cncweld_core_node");
+  ros::NodeHandle nh, pnh("~");
 
-    termios stored_settings;
-    tcgetattr(0, &stored_settings);
-    termios new_settings = stored_settings;
-    new_settings.c_lflag &= (~ICANON);
-    new_settings.c_lflag &= (~ISIG); // don't automatically handle control-C
-    new_settings.c_lflag &= ~(ECHO); // no echo
-    tcsetattr(0, TCSANOW, &new_settings);
+  // check required parameters
+  if (!pnh.hasParam("port"))
+  {
+    ROS_FATAL("Parameter 'port' missing. Cannot continue.");
+    return -1;
+  }
 
-    cout<<"\e[2J\e[1;1H"; //Clear screen and put cursor to 1;1
+  // Get parameters from launch file
+  getrosparams(pnh);
 
-    try {
-        CallbackAsyncSerial serial(argv[1],stoi(argv[2]));
-        serial.setCallback(received);
-        for(;;)
-        {
-            if(serial.errorStatus() || serial.isOpen()==false)
-            {
-                cerr<<"Error: serial port unexpectedly closed"<<endl;
-                break;
-            }
-            char c;
-            cin.get(c); //blocking wait for standard input
-            if(c==3) //if Ctrl-C
-            {
-                cin.get(c);
-                switch(c)
-                {
-                    case 3:
-                        serial.write(&c,1);//Ctrl-C + Ctrl-C, send Ctrl-C
-                    break;
-                    case 'x': //fall-through
-                    case 'X':
-                        goto quit;//Ctrl-C + x, quit
-                    default:
-                        serial.write(&c,1);//Ctrl-C + any other char, ignore
-                }
-            } else serial.write(&c,1);
-        }
-        quit:
-        serial.close();
-    } catch (std::exception& e) {
-        cerr<<"Exception: "<<e.what()<<endl;
-    }
+  // setup the serial port first 
+  // SimpleSerial serial(devname, baudrate);
+  // serial.setCallback(received);
 
-    tcsetattr(0, TCSANOW, &stored_settings);
+  // Setup GRBL with these parameters
+  setGrbl();
+
+  homeGrbl();
+
 }
