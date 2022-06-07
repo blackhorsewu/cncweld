710969906de1aaa9cfd64ee147e4ba224ba834bf
blackhorsewu <victor.wh.wu@gmail.com>
2022-06-07 16:45:18 +0800

    Just before joint state publisher added to core

--- src/cncweld_core/src/cncweld_core_node.cpp
+++ src/cncweld_core/src/cncweld_core_node.cpp
@@ -56,92 +56,15 @@ int getrosparams(ros::NodeHandle pnh)
 // Open GRBL serial port. This is a GRBL specific program.
 SimpleSerial serial("/dev/ttyACM0", 115200);
 
-/* Do not need callback for Synchronous serial
-void received(const char *data, unsigned int len)
-{
-    // cout << "Received " << len << " characters: ";
-    vector<char> v(data,data+len);
-    for(unsigned int i=0;i<v.size();i++)
-    {
-        if(v[i]=='\n')
-        {
-            cout<<endl;
-        } else {
-            if(v[i]<32 || v[i]>=0x7f) cout.put(' ');//Remove non-ascii char
-            else cout.put(v[i]);
-        }
-    }
-    cout.flush();//Flush screen buffer
-}
-*/
-
-int setGrbl()
-{
-  ROS_INFO("In setGrbl");
-  try {
-
-    // set the steps per mm
-    serial.writeString("$100=" + to_string(xsteps) + "\n");
-    cout << "Received (2): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$101=" + to_string(ysteps) + "\n");
-    cout << "Received (3): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$102=" + to_string(zsteps) + "\n");
-    cout << "Received (4): " << serial.readLine() << " : end" << endl;
-
-    ROS_INFO("Steps set.");
-
-    // set the maximum speed mm/min
-    serial.writeString("$110=" + to_string(xspeed) + "\n");
-    cout << "Received (5): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$111=" + to_string(yspeed) + "\n");
-    cout << "Received (6): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$112=" + to_string(zspeed) + "\n");
-    cout << "Received (7): " << serial.readLine() << " : end" << endl;
-
-    ROS_INFO("Speeds set.");
-
-    // set the maximum acceleration mm/sec^2
-    serial.writeString("$120=" + to_string(xaccel) + "\n");
-    cout << "Received (8): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$121=" + to_string(yaccel) + "\n");
-    cout << "Received (9): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$122=" + to_string(zaccel) + "\n");
-    cout << "Received (10): " << serial.readLine() << " : end" << endl;
-
-    ROS_INFO("Accelerations set.");
-
-    // Set the maximum travel distance mm
-    serial.writeString("$130=" + to_string(xmax - xmin) + "\n");
-    cout << "Received (11): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$131=" + to_string(ymax - ymin) + "\n");
-    cout << "Received (12): " << serial.readLine() << " : end" << endl;
-    serial.writeString("$132=" + to_string(zmax - zmin) + "\n");
-    cout << "Received (13): " << serial.readLine() << " : end" << endl;
-
-    ROS_INFO("Travels set.");
-
-  } catch (std::exception& e) {
-    cerr<<"Exception: "<<e.what()<<endl;
-  }
-
-  return(1);
-}
 
 int homeGrbl()
 {
-
-  ROS_INFO("In home.");
-
   try
   {
-    // serial.writeString("$$\n");
-    // cout << "Received (4): " << serial.readLine() << " : end" << endl;
-
-    // serial.writeString("?\n");
-    // cout << "Received: " << serial.readLine() << " : end" << endl;
-    serial.writeString("$H\n");
+    serial.writeString("$H\n"); // Send the code
+     // Wait at least for 10 seconds before expect to receive any response
     sleep(10);
-    input_line = serial.readLine();
+    input_line = serial.readLine(); // wait for
     while (input_line != "ok")
       input_line = serial.readLine();
     cout << "Received (4): " << input_line << " : end" << endl;
@@ -150,11 +73,7 @@ int homeGrbl()
   {
     std::cerr << e.what() << '\n';
   }
-  
-  ROS_INFO("Sent home.");
-
   return(1);
-
 }
 
 int main(int argc, char* argv[])
@@ -173,19 +92,13 @@ int main(int argc, char* argv[])
   getrosparams(pnh);
 
   // Before doing anything, wake up Grbl
-  ROS_INFO("waking up Grbl");
-  serial.writeString("\n");
-
+  serial.writeString("\n\n");
   sleep(2); // Wait for Grbl to initialize
-
   ROS_INFO("Grbl woke up");
   cout << "Received (1) : " << serial.readLine() << " : end" << endl;
   cout << "Received (2) : " << serial.readLine() << " : end" << endl;
   cout << "Received (3) : " << serial.readLine() << " : end" << endl;
 
-  // setGrbl(); // May be it should not be setup every time.
-
-  // ROS_INFO("Finish setGrbl, going to home.");
   homeGrbl();
 
 }
