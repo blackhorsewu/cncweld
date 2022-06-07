0ace11f1b1de9ad275d0a379640c1ae57a05a786
blackhorsewu <victor.wh.wu@gmail.com>
2022-06-07 17:52:53 +0800

    Just before AsyncSerial taken away

--- src/cncweld_core/src/cncweld_core_node.cpp
+++ src/cncweld_core/src/cncweld_core_node.cpp
@@ -1,5 +1,6 @@
 
 #include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
 
 #include <iostream>
 #include <stdio.h>
@@ -7,9 +8,12 @@
 #include <termios.h>
 
 #include "SimpleSerial.h"
+
+
 using namespace std;
 
-// ros parameters
+
+// ros parameters for GRBL
 string devname;
 int baudrate;
 
@@ -21,8 +25,22 @@ int xsteps, ysteps, zsteps;
 
 string input_line;
 
-// static int serial;
-struct termios SerialPortSettings;
+// Joint State Publisher
+ros::Publisher jsp_pub;
+
+// Create an object of type "jointState"
+sensor_msgs::JointState jointState;
+
+void initJointStates()
+{
+  jointState.name.push_back("base_link_X_link_joint");
+  jointState.name.push_back("X_link_Y_link_joint");
+  jointState.name.push_back("Y_link_Z_link_joint");
+  jointState.name.push_back("Z_link_tool0_");
+
+  for (int i=0; i < 3; i++) // allocate memory and initialize joint values
+    jointState.position.push_back(0.0);
+}
 
 int getrosparams(ros::NodeHandle pnh)
 {
@@ -81,6 +99,8 @@ int main(int argc, char* argv[])
   ros::init(argc, argv, "cncweld_core_node");
   ros::NodeHandle nh, pnh("~");
 
+  jsp_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
+
   // check required parameters
   if (!pnh.hasParam("port"))
   {
@@ -90,6 +110,8 @@ int main(int argc, char* argv[])
 
   // Get parameters from launch file
   getrosparams(pnh);
+
+  initJointStates();
 
   // Before doing anything, wake up Grbl
   serial.writeString("\n\n");
@@ -101,4 +123,28 @@ int main(int argc, char* argv[])
 
   homeGrbl();
 
+  // After 'home', do something useful
+  // The laser scanner driver should have been started by the launch file
+  // Move to the starting point for scanning, and save its position
+  // Subscribe the laser scanner point cloud
+  // Switch on the laser of the scanner
+  // An infinite loop to 
+  // 1. Query Grbl for CNC positions and publish them as jointStates
+  // 2. Exit the loop when destination reached and switch off the scanner
+  // 
+  // Instruct Grbl to go back to the starting point
+  // Construct a G-Code program for the welding track
+  // Send the G-Code program to Grbl and switch on the welding torch
+  // Go into another loop to query Grbl for positions and publish them as
+  // jointStates.
+  // Exit the loop when destination reached
+
+  // Most of the useful works should also be done in callbacks
+  // that is to handle events. One of the major events is the receipt of a
+  // point cloud from the laser scanner.
+  // 1. When /profiles point clouds callback, transform it and publish it.
+  //    At the same time, work out the Grbl state and decide if send new target
+  //    of movement.
+  // 2. Save the deepest point of every received point cloud.
+  // 3. Finish the loop when the point cloud reached the destination (end point).
 }
